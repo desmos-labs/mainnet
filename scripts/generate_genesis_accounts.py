c8751e4abf4e22185054c31b4a9a83027e198787
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-08-18 10:32:11 +0200

    Fixed how vesting accounts are generated - Vesting accounts now have the correct balance inside the genesis state

--- scripts/generate_genesis_accounts.py
+++ scripts/generate_genesis_accounts.py
@@ -15,15 +15,15 @@ class Period:
     """
 
     def __init__(self, amount: Coin, length: int):
-        self.amount = amount
-        self.length = length
+        self._amount = amount
+        self._length = length
 
     def __eq__(self, other):
         """
         Overrides the default implementation
         """
         if isinstance(other, Period):
-            return self.amount == other.amount and self.length == other.length
+            return self._amount == other._amount and self._length == other._length
         return False
 
     def to_json(self):
@@ -32,8 +32,8 @@ class Period:
         :return: A dictionary representing the JSON version of this instance.
         """
         return {
-            'length': str(self.length),
-            'amount': [self.amount.to_json()]
+            'length': str(self._length),
+            'amount': [self._amount.to_json()]
         }
 
 
@@ -42,24 +42,25 @@ class Account:
     Represents a single account.
     """
 
-    def __init__(self, address: str, balance: Coins, periods: [Period], total_vested: Coins):
-        self.address = address
-        self.balance = balance
-        self.periods = periods
-        self.total_vested = total_vested
+    def __init__(self, address: str, balance: Coins, periods: [Period]):
+        self._address = address
+        self._balance = balance
+        self._periods = periods
 
     def __eq__(self, other):
         """Overrides the default implementation"""
         if isinstance(other, Account):
-            return self.address == other.address and self.periods == other.periods
+            return self._address == other._address and self._periods == other._periods
         return False
 
-    def get_balance(self) -> Balance:
-        """
-        Returns the balance associated to this account.
-        :return: The Balance instance representing the no-lockup balance associated to this account.
-        """
-        return Balance(self.address, self.balance)
+    def get_address(self) -> str:
+        return self._address
+
+    def get_balance(self):
+        return self._balance
+
+    def get_periods(self):
+        return self._periods
 
     def to_json(self, genesis_time: str) -> dict:
         """
@@ -67,7 +68,7 @@ class Account:
         :return: A dictionary containing this information as a JSON object.
         """
 
-        if len(self.periods) > 0:
+        if len(self._periods) > 0:
             return self._get_periodic_vesting_account(genesis_time)
         else:
             return self._get_base_account()
@@ -75,7 +76,7 @@ class Account:
     def _get_base_account(self):
         return {
             '@type': '/cosmos.auth.v1beta1.BaseAccount',
-            'address': self.address,
+            'address': self._address,
             'pub_key': None,
             'account_number': '0',
             'sequence': '0'
@@ -83,25 +84,25 @@ class Account:
 
     def _get_end_time(self) -> int:
         end_time = 0
-        for period in self.periods:
-            end_time += period.length
+        for period in self._periods:
+            end_time += period._length
         return end_time
 
     def _get_periodic_vesting_account(self, genesis_time: str):
         json_periods = []
-        for period in self.periods:
+        for period in self._periods:
             json_periods.append(period.to_json())
         start_time = int(iso8601.parse_date(genesis_time).timestamp())
         return {
             '@type': '/cosmos.vesting.v1beta1.PeriodicVestingAccount',
             'base_vesting_account': {
                 'base_account': {
-                    "address": self.address,
+                    "address": self._address,
                     "pub_key": None,
                     "account_number": '0',
                     "sequence": '0'
                 },
-                'original_vesting': self.total_vested.to_json(),
+                'original_vesting': self._balance.to_json(),
                 'delegated_free': [],
                 'delegated_vesting': [],
                 'end_time': str(start_time + self._get_end_time())
@@ -117,31 +118,37 @@ class Entry:
     """
 
     def __init__(self, csv_row):
-        self.address = csv_row['Address']
-        self.investors_incentives = int(csv_row['Vesting Investors Incentives'])
-        self.uaf = int(csv_row['Vesting UAF'])
-        self.foundation_ventures = int(csv_row['Vesting Entities'])
-        self.teammates_advisors_early_supporters = int(csv_row['Vesting Teammates/Advisors/Supporters'])
-        self.no_vesting = int(csv_row['No Vesting'])
-        self.total_vested = int(csv_row['Total Vested'])
+        self._address = csv_row['Address']
+        self._investors_incentives = int(csv_row['Vesting Investors Incentives'])
+        self._uaf = int(csv_row['Vesting UAF'])
+        self._foundation_ventures = int(csv_row['Vesting Entities'])
+        self._teammates_advisors_early_supporters = int(csv_row['Vesting Teammates/Advisors/Supporters'])
+        self._no_vesting = int(csv_row['No Vesting'])
+        self._total = int(csv_row['Total'])
 
     def get_address(self) -> str:
-        return self.address
+        return self._address
 
-    def get_total_vested(self) -> int:
-        return self.total_vested
-
-    def get_no_vested(self) -> int:
-        return self.no_vesting
+    def get_balance(self) -> Coins:
+        return Coins([Coin(self._total, COIN_DENOM)])
 
     def get_periods(self) -> [Period]:
         periods = []
+
+        # If there are no tokens to be vested, return an empty slice of periods
+        if self._total == self._no_vesting:
+            return []
+
+        # Amount of tokens that will not be vested
+        no_vested = self._no_vesting
+        if no_vested > 0:
+            periods.append(Period(Coin(no_vested, COIN_DENOM), 0))
 
         # Amount of tokens that will be vested:
         # - 33% at end of 12th month
         # - 5.58% each month for 11 months
         # - 5.62% at the end of the 24th month
-        vested_33 = self.investors_incentives
+        vested_33 = self._investors_incentives
         if vested_33 > 0:
             # Append the first period for the 12th month
             vested_33_month_12 = int(vested_33 * 0.33)
@@ -160,7 +167,7 @@ class Entry:
         # - 50% at the end of the 24th month
         # - 2.08% each month for 23 months
         # - 2.16% at the end of the 48th month
-        vested_50 = self.teammates_advisors_early_supporters
+        vested_50 = self._teammates_advisors_early_supporters
         if vested_50 > 0:
             # Append the first period for the 24th month
             vested_50_month_24 = int(vested_50 * 0.5)
@@ -178,7 +185,7 @@ class Entry:
         # Amount of tokens that will be vested:
         # - 4.16% each month for 23 months
         # - 4.32% at the 24th month
-        vested_uaf = self.uaf
+        vested_uaf = self._uaf
         if vested_uaf > 0:
             # Append 23 periods for month 1 to 23 included
             vested_uaf_month_1_23 = int(vested_uaf * 0.0416)
@@ -194,7 +201,7 @@ class Entry:
         # - 25% at end of 42th month
         # - 25% at end of 45th month
         # - 25% at end of 48th month
-        vested_entities = self.foundation_ventures
+        vested_entities = self._foundation_ventures
         if vested_entities > 0:
             amount = int(vested_entities * 0.25)
             periods.extend([
@@ -207,6 +214,17 @@ class Entry:
         return periods
 
 
+def convert_entry(entry: Entry) -> Account:
+    """
+    Allows to convert the given entry to an Account instance.
+    """
+    return Account(
+        entry.get_address(),
+        entry.get_balance(),
+        entry.get_periods(),
+    )
+
+
 def read_csv(file_path: str) -> [Account]:
     """
     Reads the CSV file at the given path as a list of accounts.
@@ -217,17 +235,9 @@ def read_csv(file_path: str) -> [Account]:
     with open(file_path, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
-        # Skip the first line as it contains the column names
-
         # Read the lines
         for line in csv_reader:
-            entry = Entry(line)
-            accounts.append(Account(
-                entry.get_address(),
-                Coins([Coin(entry.get_no_vested(), COIN_DENOM)]),
-                entry.get_periods(),
-                Coins([Coin(entry.get_total_vested(), COIN_DENOM)]),
-            ))
+            accounts.append(convert_entry(Entry(line)))
 
     return accounts
 
@@ -246,8 +256,8 @@ def write_accounts(accounts: [Account], genesis_file_path: str):
         json_accounts = list(map(lambda account: account.to_json(genesis_time), accounts))
         genesis['app_state']['auth']['accounts'] = json_accounts
 
-        non_zero_balances = filter(lambda account: not account.get_balance().is_zero(), accounts)
-        json_balances = list(map(lambda account: account.get_balance().to_json(), non_zero_balances))
+        balances = list(map(lambda account: Balance(account.get_address(), account.get_balance()), accounts))
+        json_balances = list(map(lambda balance: balance.to_json(), balances))
         genesis['app_state']['bank']['balances'] = json_balances
 
     with open(genesis_file_path, mode='w') as genesis_file:
